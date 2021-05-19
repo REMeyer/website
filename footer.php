7303eb7db7157a27d40261463e9664671fb5c31b
Ryan Meyer <meyer@asmaclap04.home>
2021-05-19 10:57:01 +0100

    Adjusted dimensions and overall look

--- footer.php
+++ footer.php
@@ -1,6 +1,6 @@
 <!DOCTYPE html>
 
 <div id="footer">
-	<p> &#169; R Elliot Meyer 2015 | Original Design by <a href="http://templated.co/reddened" rel="nofollow">TEMPLATED</a></p>
+	<p> &#169; R Elliot Meyer 2021 | Original Design by <a href="http://templated.co/reddened" rel="nofollow">TEMPLATED</a></p>
 </div>
 
